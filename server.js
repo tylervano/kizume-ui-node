112f3f6d0131546067f5252fa37d34d423f118f4
Tyler <tyler.vano@gmail.com>
2016-03-07 22:54:49 -0800

    Updating server.js

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
-var express = require('express');
-var app = express();
+var express	= require('express');
+var app		= express();
+var fs		= require('fs');
+var parser	= require('body-parser');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//Setup ip adress and port
+var ipaddress ;
 
+
+/**
+ * Get the IP address from the environment variables.
+ *
+ * @return {String}
+ */
+function initIPAdress() {
+    var adr = process.env.OPENSHIFT_NODEJS_IP;
+    if (typeof adr === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using localhost');
+            adr = 'localhost';
+    }
+
+    ipaddress = adr;
+}
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+
+/**
+ * Basic landing page.
+ *
+ * @return null
+ */
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!')
 });
 
-app.listen(server_port, server_ip_address, function () {
-  console.log('Example app listening on ' + server_ip_address + ', port ' + server_port + '!');
+
+/**
+ * Get admin panel from disk.
+ *
+ * @return null
+ */
+ 
+/*
+app.get('/admin', function (req, res) {
+        res.setHeader('Content-Type', 'text/html'); 
+        res.send( fs.readFileSync('./index_admin.html') );
+});
+*/
+
+initIPAdress(); //Setup IP adress before app.listen()
+
+app.listen(port, ipaddress, function() {
+        console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), ipaddress, port);
 });
